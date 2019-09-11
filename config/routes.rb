fcd99402eb4db555bd6ef0c9c9c4692713450f64
shota <ppddccaa165@gmail.com>
2019-09-11 21:08:45 +0900

    test left

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tops#top'
+  get "about" => "tops#about"
   resources :books
   resources :users, only: [:edit, :update, :show, :index]
   # For details on the DSL available within this file, see http://guides.ruyonrails.org/routing.html
